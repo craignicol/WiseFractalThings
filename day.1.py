14c0787de9caa842c7040390a4a7baa179f8db80
craignicol <craig.nicol@scotalt.net>
2019-12-04 23:57:44 +0000

    Day 1, part 2

--- day.1.py
+++ day.1.py
@@ -23,16 +23,42 @@ The Fuel Counter-Upper needs to know the total fuel requirement. To find it, ind
 What is the sum of the fuel requirements for all of the modules on your spacecraft?
 """
 
+rules2 = """
+--- Part Two ---
+During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+
+Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+
+So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+
+A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
+"""
+
 def fuel_required(mass):
   return (mass // 3) - 2
+
+def fuel_required_including_fuel(mass):
+  running_total = 0
+  extra_fuel = fuel_required(mass)
+  while (extra_fuel > 0):
+    running_total += extra_fuel
+    extra_fuel = fuel_required(extra_fuel)
+  return running_total
 
 def total_fuel_required(mass_list):
   return sum([fuel_required(m) for m in mass_list])
 
+def total_fuel_required_including_fuel(mass_list):
+  return sum([fuel_required_including_fuel(m) for m in mass_list])
+
 def execute():
     with open('input.1.txt') as inp:
         lines = inp.readlines()
-    return total_fuel_required([int(l.strip()) for l in lines if len(l.strip()) > 0])
+    masses = [int(l.strip()) for l in lines if len(l.strip()) > 0]
+    return total_fuel_required(masses), total_fuel_required_including_fuel(masses)
 
 def verify(a, b):
     if (a == b):
@@ -47,6 +73,11 @@ def test_cases():
     verify(654, fuel_required(1969))
     verify(33583, fuel_required(100756))
     verify(2+2+654+33583, total_fuel_required([12, 14, 1969, 100756]))
+    verify(2, fuel_required_including_fuel(12))
+    verify(2, fuel_required_including_fuel(14))
+    verify(966, fuel_required_including_fuel(1969))
+    verify(50346, fuel_required_including_fuel(100756))
+    verify(2+2+966+50346, total_fuel_required_including_fuel([12, 14, 1969, 100756]))
 
 if __name__ == "__main__":
     test_cases()
