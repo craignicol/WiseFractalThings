c49eb712384e69ce0054289a3cb00697503b3220
craignicol <craig.nicol@scotalt.net>
2019-12-05 00:27:03 +0000

    Day 2 part 2

--- day.2.py
+++ day.2.py
@@ -6,6 +6,8 @@ OPCODE_END = 99
 
 input2 = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,19,6,23,2,13,23,27,1,9,27,31,2,31,9,35,1,6,35,39,2,10,39,43,1,5,43,47,1,5,47,51,2,51,6,55,2,10,55,59,1,59,9,63,2,13,63,67,1,10,67,71,1,71,5,75,1,75,6,79,1,10,79,83,1,5,83,87,1,5,87,91,2,91,6,95,2,6,95,99,2,10,99,103,1,103,5,107,1,2,107,111,1,6,111,0,99,2,14,0,0]
 
+target_output = 19690720
+
 def intcode_run(ram):
     if (len(ram) < 1) :
         return ram
@@ -24,9 +26,16 @@ def intcode_run(ram):
     return ram
 
 def execute():
-    input2[1] = 12
-    input2[2] = 2
-    return intcode_run(input2)[0]
+    x = 0
+    y = 0
+    for x in range(99):
+        for y in range(99):
+            ram = input2[:]
+            ram[1] = x
+            ram[2] = y
+            if (intcode_run(ram)[0] == target_output):
+                return x * 100 + y
+        print("{}...".format(x))
 
 def verify(a, b):
     if (a == b):
