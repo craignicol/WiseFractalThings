f22a3be67aabd6877b70eeb74000b7e25c236a5b
craignicol <craig.nicol@scotalt.net>
2019-12-10 00:44:06 +0000

    Day 6, Part 2, Test data

--- day.6.py
+++ day.6.py
@@ -3,7 +3,8 @@
 def execute():
     with open('input.6.txt') as inp:
         lines = inp.readlines()
-    return count_orbits([l.strip() for l in lines])
+    orbits = [l.strip() for l in lines]
+    return count_orbits(orbits), shortest_route(orbits)
 
 def verify(a, b):
     if (a == b):
@@ -26,7 +27,28 @@ E)J
 J)K
 K)L"""
 
-def count_routes(input1):
+input2 = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"""
+
+def shortest_route(input1):
+    START = "YOU"
+    END = "SAN"
+    cleansed = [o.split(")") for o in input1]
+    planets = measure_routes(cleansed)
+    return planets[START], planets[END]
+
+def measure_routes(input1):
     planets = dict({(x[1], 0) for x in input1})
     next_planets = [x[1] for x in input1 if x[0] == ROOT_NODE]
     distance = 1
@@ -35,7 +57,11 @@ def count_routes(input1):
             planets[ip] = distance
         distance += 1
         next_planets = [x[1] for x in input1 if x[0] in next_planets]
+    
+    return planets
 
+def count_routes(input1):
+    planets = measure_routes(input1)
     return sum(planets.values())
 
 def count_orbits(input1):
@@ -45,6 +71,7 @@ def count_orbits(input1):
 def test_cases():
     verify(1, count_orbits(["COM)B"]))
     verify(42, count_orbits(input1.splitlines()))
+    verify(4, shortest_route(input2.splitlines()))
 
 if __name__ == "__main__":
     test_cases()
